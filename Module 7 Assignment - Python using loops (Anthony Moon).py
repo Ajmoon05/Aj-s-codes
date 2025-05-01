ce53868e84025d7336eb4a795134a5475d7a0a5a
Ajmoon05 <choc3.14159@yahoo.com>
2025-04-30 19:40:47 -0500

    Add files via upload

---
+++ Module 7 Assignment - Python using loops (Anthony Moon).py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 40, 55, 109]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")
